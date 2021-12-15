bf6577ecb185797cca525799720b339856b20a3f
HuyHa <hahuy98mh@gmail.com>
2021-12-16 00:14:49 +0700

    Learn variable

---
+++ Bai1_13-12/huy.js
@@ -0,0 +1,6 @@
+var a = 5;
+var b = 10;
+var result = a + b;
+var tich = a * b;
+console.log("tong = ", result)
+console.log ("tich =",tich)
---
+++ Bai1_13-12/index.js
@@ -0,0 +1,27 @@
+// var name = "Huy" // string
+// var ageHuy = 23 // number
+// var isMyFriend = true // boolean
+// var grade = 8.5 // number
+// var className // undefined
+// var ageEmkemHuy = 5
+// var ageChildrenHuy = 2
+
+/*var  ageHuy = 23;
+var ageToan = 23;
+if(ageHuy > ageToan) {
+    console.log("Huy la anh Toan")
+}
+else if(ageHuy == ageToan) {-
+    console.log("la ban")
+}
+else {
+    console.log("Toan la anh Huy")
+}*/
+
+// Dau vao chieu dai va chieu rong => dau ra tinh dien tich va chu vi hinh chu nhat
+// dau vao 2 so => tinh cong, tru, nhan, chia
+// dau vao la diem thi cua Huy => Xem no la xep loai gi (Gioi >= 9, kha >7, con lai trung binh)
+var diemHuy = 5;
+if (diemHuy >= 9) { onsole.log("Gioi") }
+else if (diemHuy >> 7) { console.log("kha") }
+else { console.log("trung binh") }
