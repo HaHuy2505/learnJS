caddd2f22a7b52ac980963a02687af66a83a099f
HuyHa <hahuy98mh@gmail.com>
2021-12-16 03:00:56 +0700

    baitapvn

---
+++ Bai3_15-12/bai6.js
@@ -0,0 +1,13 @@
+// var n = 2;
+// var ketqua = 0;
+// for (var i = 1; i <= n; i++);
+// {
+//   ketqua = ketqua + 1/(i*(i+1));
+//   console.log("S(n) = ", ketqua)
+// }
+var n=1;
+var ketqua =0;
+for (var i=0 ;i<=n ;i++){
+    ketqua = ketqua +(2*i+1)/(2*i+2);
+}
+ console.log (ketqua)
---
+++ Bai3_15-12/bai7.js
@@ -0,0 +1,14 @@
+// var n=3 ;
+// var ketqua =0;
+// for (var i=1 ;i<=n ;i++){
+//     ketqua = ketqua + i/(i+1);
+// }
+//     console.log ("S=",ketqua)
+
+// bai 8
+var n=3 ;
+var ketqua =0;
+for (var i=0 ;i<=n ; i++){
+    ketqua =ketqua+(2*i+1)/(2*i+2);
+}
+console.log (ketqua)
